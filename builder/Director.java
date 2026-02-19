5bc4668814f2f684a31fd8e22727656ffd690442
Xiomara ! <xiomarafuentes1811@gmail.com>
2026-02-19 12:46:11 -0500

    feat: normal burger method implementations

--- builder/Director.java
+++ builder/Director.java
@@ -1,11 +1,17 @@
 package builder;
 
 public class Director {
-    public void buildNormalBurger(){
 
+    public void buildNormalBurger(HamburgerBuild builder){
+        //For burgers without extra ingredients.
+        builder.reset();
+        builder.buildBread();
+        builder.buildMeat();
+        builder.buildCheese();
     }
 
     public void buildExtraIngredients(HamburgerBuild builder){
+        //Implementation of the methods.
         builder.reset();
         builder.buildBread();
         builder.buildMeat();
